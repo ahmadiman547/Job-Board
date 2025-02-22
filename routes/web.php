0fe23ce32000fb4cbbfcfed1f4ef3786c9b60c3c
Ahmad Iman <ahmadiman007@gmail.com>
2025-02-22 12:23:25 +0800

    Current progress

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,49 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+// Create
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+// Show
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+// Store
+Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 301
+    ]);
+
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
