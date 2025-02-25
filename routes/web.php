de22183e8e5c17a004d7206f20162249e175734e
Ahmad Iman <ahmadiman007@gmail.com>
2025-02-25 17:19:29 +0800

    Implement user authentication (login/logout/registration)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
@@ -18,4 +20,10 @@ Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
 
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
